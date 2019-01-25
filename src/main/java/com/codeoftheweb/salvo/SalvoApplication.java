3301ee8a3c8266692a44f0c7605ea02d869f819f
TobiasHeimboeck <tobiasheimboeck@outlook.com>
2019-01-25 09:51:56 +0100

    First commit of game project

---
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,24 @@
+package com.codeoftheweb.salvo;
+
+import com.codeoftheweb.salvo.models.Player;
+import com.codeoftheweb.salvo.repository.PlayerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initData(PlayerRepository repository) {
+        return (args) -> {
+            repository.save(new Player("tobiasheimboeck@outlook.com"));
+        };
+    }
+}
+
